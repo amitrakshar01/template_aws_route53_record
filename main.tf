b74f8be5d0c271938695dd01a90d550a93907bce
AksharSans <sanyalamitrakshar@gmail.com>
2022-11-24 11:21:39 +0530

    Added template

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "example.com"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "www.example.com"
+  type    = "A"
+  ttl     = 300
+}
