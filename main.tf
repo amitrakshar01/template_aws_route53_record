5db208830dc28e80f81337daaacfc26f1e055722
AksharSans <sanyalamitrakshar@gmail.com>
2022-11-26 21:19:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "aws_route53_zone" "primary" {
   name = "example.com"
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "txt" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www.example.com"
-  type    = "A"
+  type    = "TXT"
   ttl     = 200
 }
